ef179f1a452be50d717e5f533e1efd023359108b
yjindata <hf746@naver.com>
2019-02-18 12:14:29 +0900

    Remove auth

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,44 +1,17 @@
 import React, { Component } from 'react';
 import Chat from './Chat';
-import Explain from './HomeExplain';
+import HomeExplain from './HomeExplain'
 import BarChart from './BarChart';
 
 class Home extends Component {
-  login = () => {
-    this.props.auth.login();
-  }
-  logout = () => {
-    this.props.auth.logout();
-  }
   render() {
-    const { isAuthenticated } = this.props.auth;
     return (
       <div>
-        {
-          isAuthenticated() &&
-          <div className="container column">
-            <h5>
-              <a href style={{ cursor: 'pointer' }}
-                onClick={this.logout}
-              >
-              Log Out
-              </a>
-            </h5>
-            <Chat />
-            <BarChart />
-          </div>}
-        
-        {
-          !isAuthenticated() && (
-            <div className="container column">
-              <h1>Avodado market</h1>
-
-              <Explain />
-              <Chat />
-            </div>
-          )
-        }
-      </div> 
+          <HomeExplain />
+          <Chat />
+          <BarChart />
+          </div>
+      
       );
     }
   }
